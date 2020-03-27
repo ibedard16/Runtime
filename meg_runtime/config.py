1c41787ed5432bb4d95e4653434fda8feb1156e1
Kyle Pugh <kyletpugh@users.noreply.github.com>
2020-03-27 01:10:05 -0400

    Enforce some better rules in the configuration with keys

--- meg_runtime/config.py
+++ meg_runtime/config.py
@@ -6,8 +6,19 @@ Configuration for runtime
 import os
 import re
 import json
+import shutil
 from pathlib import Path
 from kivy.logger import Logger
+
+
+# Runtime configuration exception
+class ConfigException(Exception):
+    """Runtime configuration exception"""
+
+    # Runtime configuration exception constructor
+    def __init__(self, message, **kwargs):
+        """Runtime configuration exception constructor"""
+        super().__init__(message, **kwargs)
 
 
 # Runtime configuration
@@ -16,6 +27,11 @@ class Config(dict):
 
     # The singleton configuration instance
     __instance = None
+    # The block list of keys to prevent setting
+    __blocked_keys = [
+        'path/config',
+        'path/user'
+    ]
 
     # Configuration constructor
     def __init__(self, **kwargs):
@@ -23,7 +39,7 @@ class Config(dict):
         # Check if there is already a configuration instance
         if Config.__instance is not None:
             # Except if another instance is created
-            raise Exception(self.__class__.__name__ + " is a singleton!")
+            raise ConfigException(self.__class__.__name__ + " is a singleton!")
         else:
             # Initialize super class constructor
             super().__init__(**kwargs)
@@ -34,28 +50,34 @@ class Config(dict):
 
     # Load a configuration from file
     @staticmethod
-    def load(path):
-        """Load a configuration from file"""
+    def load(path, clear=True):
+        """Load a configuration from JSON file"""
         # Check there is a configuration instance
         if Config.__instance is None:
             Config()
-        if Config.__instance is not None:
-            # Clear the old configuration before loading a new one
+        if Config.__instance is None:
+            return False
+        # Clear the old configuration before loading a new one, if wanted
+        if clear:
             Config.clear()
-            try:
-                # Get the configuration path
-                expanded_path = Config.expand(path)
-                Logger.debug(f'MEG Config: Loading configuration <{expanded_path}>')
-                # Try to open the configuration file for reading
-                config_file = open(expanded_path, "r")
-                # Try to parse the JSON configuration file
-                config = json.load(config_file)
-                # Overwrite or update the configuration
-                Config.__instance.update(config)
-            except Exception as e:
-                # Log that loading the configuration failed
-                Logger.warning(f'MEG Config: {e}')
-                Logger.warning(f'MEG Config: Could not load configuration <{expanded_path}>')
+        try:
+            # Get the configuration path
+            expanded_path = Config.expand(path)
+            Logger.debug(f'MEG Config: Loading configuration <{expanded_path}>')
+            # Try to open the configuration file for reading
+            config_file = open(expanded_path, "r")
+            # Try to parse the JSON configuration file
+            config = json.load(config_file)
+            # Overwrite or update the configuration
+            Config.__instance.update(config)
+            # Keep the correct path for configuration
+            Config.__instance['path']['config'] = path
+        except Exception as e:
+            # Log that loading the configuration failed
+            Logger.warning(f'MEG Config: {e}')
+            Logger.warning(f'MEG Config: Could not load configuration <{expanded_path}>')
+            return False
+        return True
 
     # Save a configuration to file
     @staticmethod
@@ -64,24 +86,31 @@ class Config(dict):
         # Check there is a configuration instance
         if Config.__instance is None:
             Config()
-        if Config.__instance is not None:
-            try:
-                Logger.debug(f'MEG Config: Saving configuration <{Config.expand(path)}>')
-                # Check the file exists before overwriting
-                if not overwrite and os.path.exists(path):
-                    raise Exception(f'Not overwriting existing file <{path}>')
-                # Try to open the configuration file for writing
-                config_file = open(path, "w")
-                # Try to convert configuration to JSON file
-                json.dump(Config.__instance, config_file)
-            except Exception as e:
-                # Log that saving the configuration failed
-                Logger.warning(f'MEG Config: {e}')
-                Logger.warning(f'MEG Config: Could not save configuration <{Config.expand(path)}>')
+        if Config.__instance is None:
+            return False
+        try:
+            Logger.debug(f'MEG Config: Saving configuration <{Config.expand(path)}>')
+            # Check the file exists before overwriting
+            if not overwrite and os.path.exists(path):
+                raise ConfigException(f'Not overwriting existing file <{path}>')
+            # Make the path to the containing directory if it does not exist
+            dir_path = os.path.dirname(path)
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path, exist_ok=True)
+            # Try to open the configuration file for writing
+            config_file = open(path, "w")
+            # Try to convert configuration to JSON file
+            json.dump(Config.__instance, config_file)
+        except Exception as e:
+            # Log that saving the configuration failed
+            Logger.warning(f'MEG Config: {e}')
+            Logger.warning(f'MEG Config: Could not save configuration <{Config.expand(path)}>')
+            return False
+        return True
 
     # Expand a configuration value with configuration references
     @staticmethod
-    def expand(value, keys=[]):
+    def expand(value, exclude_keys=[]):
         """Expand a configuration value with configuration references"""
         # Only expand strings
         if isinstance(value, str):
@@ -94,13 +123,13 @@ class Config(dict):
                 for m in matches:
                     # Replace the dictionary reference with the value
                     m_stripped = m.strip()
-                    value = value.replace('$(' + m + ')', '' if m_stripped in keys else Config.get(m_stripped, keys=keys))
+                    value = value.replace('$(' + m + ')', '' if m_stripped in exclude_keys else Config.get(m_stripped, exclude_keys=exclude_keys))
         # Return original value or expanded configuration value
         return value
 
     # Get a configuration value
     @staticmethod
-    def get(key, defaultValue='', keys=[]):
+    def get(key, defaultValue='', exclude_keys=[]):
         """Get a configuration value"""
         # Check the configuration dictionary is valid
         if Config.__instance is None:
@@ -110,22 +139,34 @@ class Config(dict):
         # Check the key is in the configuration dictionary by splitting into individual parts
         current_dict = Config.__instance
         subkeys = key.split('/')
+        # Check to make sure the key was valid or return the default value
+        subkeys[:] = [sk for sk in subkeys if sk]
+        if not len(subkeys) > 0:
+            return defaultValue
         # Go through each key and traverse the configuration dictionary
-        for sk in subkeys:
+        for sk in subkeys[:-1]:
             # Check current key is in current dictionary
             if sk not in current_dict:
                 return defaultValue
             # Get the dictionary element by key
             current_dict = current_dict[sk]
-        # Add the key to the keys list to prevent recursion
-        if isinstance(keys, list):
-            expanded_keys = keys.copy()
+            # Check the instance of the element is a dictionary or the key cannot be valid
+            if not isinstance(current_dict, dict):
+                return defaultValue
+        # Get the value of the key
+        sk = subkeys[-1]
+        if sk not in current_dict:
+            return defaultValue
+        value = current_dict[sk]
+        # Add the key to the exclude keys list to prevent recursion
+        if isinstance(exclude_keys, list):
+            expanded_keys = exclude_keys.copy()
         else:
             expanded_keys = []
         if key not in expanded_keys:
             expanded_keys.append(key)
         # Return the (possibly expanded) dictionary element
-        return Config.expand(current_dict, expanded_keys)
+        return Config.expand(value, expanded_keys)
 
     # Check a configuration key exists
     @staticmethod
@@ -139,15 +180,22 @@ class Config(dict):
         # Check the key is in the configuration dictionary by splitting into individual parts
         current_dict = Config.__instance
         subkeys = key.split('/')
+        # Check to make sure the key was valid
+        subkeys[:] = [sk for sk in subkeys if sk]
+        if not len(subkeys) > 0:
+            return False
         # Go through each key and traverse the configuration dictionary
-        for sk in subkeys:
+        for sk in subkeys[:-1]:
             # Check current key is in current dictionary
             if sk not in current_dict:
                 return False
             # Get the dictionary element by key
             current_dict = current_dict[sk]
-        # Key was found in configuration
-        return True
+            # Check the instance of the element is a dictionary or the key cannot be valid
+            if not isinstance(current_dict, dict):
+                return False
+        # Key was found in configuration if in final dictionary
+        return subkeys[-1] in current_dict
 
     # Set a configuration value
     @staticmethod
@@ -156,24 +204,35 @@ class Config(dict):
         # Check the configuration dictionary is valid
         if Config.__instance is None:
             Config()
-        if isinstance(key, str) and Config.__instance is not None:
-            # Check the value is valid or remove the key
-            if value is None:
-                # Remove the key if the value is invalid
-                Config.remove(key)
-            else:
-                # Check the key is in the configuration dictionary by splitting into individual parts
-                current_dict = Config.__instance
-                keys = key.split('/')
-                # Go through each key and traverse the configuration dictionary
-                for k in keys[:-1]:
-                    # Check current key is in current dictionary
-                    if k not in current_dict:
-                        current_dict[k] = {}
-                    # Get the dictionary element by key
-                    current_dict = current_dict[k]
-                # Set the dictionary element value
-                current_dict[keys[-1]] = value
+        if not isinstance(key, str) or Config.__instance is None:
+            return False
+        # Check the value is valid or remove the key
+        if value is None:
+            # Remove the key if the value is invalid
+            return Config.remove(key)
+        # Check the key is in the configuration dictionary by splitting into individual parts
+        current_dict = Config.__instance
+        subkeys = key.split('/')
+        # Check to make sure the key was valid or return the default value
+        subkeys[:] = [sk for sk in subkeys if sk]
+        if not len(subkeys) > 0:
+            return False
+        # Check key is blocked
+        if '/'.join(subkeys) in Config.__blocked_keys:
+            return False
+        # Go through each key and traverse the configuration dictionary
+        for sk in subkeys[:-1]:
+            # Check current key is in current dictionary
+            if sk not in current_dict:
+                current_dict[sk] = {}
+            # Get the dictionary element by key
+            current_dict = current_dict[sk]
+            # Check the instance of the element is a dictionary or the key cannot be valid
+            if not isinstance(current_dict, dict):
+                return False
+        # Set the dictionary element value
+        current_dict[subkeys[-1]] = value
+        return True
 
     # Remove a configuration value
     @staticmethod
@@ -182,21 +241,30 @@ class Config(dict):
         # Check the configuration dictionary is valid
         if Config.__instance is None:
             Config()
-        if isinstance(key, str) and Config.__instance is not None:
-            # Check the key is in the configuration dictionary by splitting into individual parts
-            current_dict = Config.__instance
-            keys = key.split('/')
-            # Go through each key and traverse the configuration dictionary
-            for k in keys[:-1]:
-                # Check current key is in current dictionary
-                if k not in current_dict:
-                    return
-                # Get the dictionary element by key
-                current_dict = current_dict[k]
-            # Remove the dictionary element
-            k = keys[-1]
-            if k in current_dict:
-                current_dict.pop(k)
+        if not isinstance(key, str) or Config.__instance is None:
+            return False
+        # Check the key is in the configuration dictionary by splitting into individual parts
+        current_dict = Config.__instance
+        subkeys = key.split('/')
+        # Check to make sure the key was valid or return the default value
+        subkeys[:] = [sk for sk in subkeys if sk]
+        if not len(subkeys) > 0:
+            return True
+        # Go through each key and traverse the configuration dictionary
+        for sk in subkeys[:-1]:
+            # Check current key is in current dictionary
+            if sk not in current_dict:
+                return True
+            # Get the dictionary element by key
+            current_dict = current_dict[sk]
+            # Check the instance of the element is a dictionary or the key cannot be valid
+            if not isinstance(current_dict, dict):
+                return True
+        # Remove the dictionary element
+        sk = subkeys[-1]
+        if sk in current_dict:
+            current_dict.pop(sk)
+        return True
 
     # Clear the configuration
     @staticmethod
@@ -210,33 +278,17 @@ class Config(dict):
             if Config.__instance is not None:
                 # Clear the dictionary
                 super(Config, Config.__instance).clear()
+            # Create the path dictionary
+            Config.__instance['path'] = {}
             # Get the user path from the environment, if present, otherwise use the default path
-            if 'MEG_USER_PATH' in os.environ:
-                Config.set('path/user', os.environ['MEG_USER_PATH'])
-            else:
-                Config.set('path/user', str(Path.home()))
+            Config.__instance['path']['user'] = str(Path.home()) if 'MEG_USER_PATH' not in os.environ else os.environ['MEG_USER_PATH']
             # Get the home path from the environment, if present, otherwise use the default path
-            if 'MEG_HOME_PATH' in os.environ:
-                Config.set('path/home', os.environ['MEG_HOME_PATH'])
-            else:
-                Config.set('path/home', '$(path/user)' + os.sep + '.meg')
+            Config.__instance['path']['home'] = '$(path/user)' + os.sep + '.meg' if 'MEG_HOME_PATH' not in os.environ else os.environ['MEG_HOME_PATH']
             # Get the cache path from the environment, if present, otherwise use the default path
-            if 'MEG_CACHE_PATH' in os.environ:
-                Config.set('path/cache', os.environ['MEG_CACHE_PATH'])
-            else:
-                Config.set('path/cache', '$(path/home)' + os.sep + 'cache')
+            Config.__instance['path']['cache'] = '$(path/home)' + os.sep + 'cache' if 'MEG_CACHE_PATH' not in os.environ else os.environ['MEG_CACHE_PATH']
             # Get the plugins path from the environment, if present, otherwise use the default path
-            if 'MEG_PLUGINS_PATH' in os.environ:
-                Config.set('path/plugins', os.environ['MEG_PLUGINS_PATH'])
-            else:
-                Config.set('path/plugins', '$(path/home)' + os.sep + 'plugins')
+            Config.__instance['path']['plugins'] = '$(path/home)' + os.sep + 'plugins' if 'MEG_PLUGINS_PATH' not in os.environ else os.environ['MEG_PLUGINS_PATH']
             # Get the plugin cache path from the environment, if present, otherwise use the default path
-            if 'MEG_PLUGIN_CACHE_PATH' in os.environ:
-                Config.set('path/plugin_cache', os.environ['MEG_PLUGIN_CACHE_PATH'])
-            else:
-                Config.set('path/plugin_cache', '$(path/home)' + os.sep + 'plugin_cache')
+            Config.__instance['path']['plugin_cache'] = '$(path/home)' + os.sep + 'plugin_cache' if 'MEG_PLUGIN_CACHE_PATH' not in os.environ else os.environ['MEG_PLUGIN_CACHE_PATH']
             # Load configuration from the environment, if present, otherwise use the default path
-            if 'MEG_CONFIG_PATH' in os.environ:
-                Config.set('path/config', os.environ['MEG_CONFIG_PATH'])
-            else:
-                Config.set('path/config', '$(path/home)' + os.sep + 'config.json')
+            Config.__instance['path']['config'] = '$(path/home)' + os.sep + 'config.json' if 'MEG_CONFIG_PATH' not in os.environ else os.environ['MEG_CONFIG_PATH']
--- tests/test_config.py
+++ tests/test_config.py
@@ -12,9 +12,9 @@ from meg_runtime import Config
 # Configuration set/get test
 def test_config_set_get():
     """Configuration set/get test"""
-    Config.set('/path/to/key', 'value')
+    assert Config.set('/path/to/key', 'value')
     assert Config.get('/path/to/key') == 'value'
-    Config.set('key/path', '$(key/path)value')
+    assert Config.set('key/path', '$(key/path)value')
     assert Config.get('key/path') == 'value'
 
 
@@ -22,15 +22,15 @@ def test_config_set_get():
 def test_config_set_expand():
     """Configuration set/expand test"""
     # Test set/expand
-    Config.set('/path/to/key', 'value')
+    assert Config.set('/path/to/key', 'value')
     assert Config.expand('$(/path/to/key)') == 'value'
     # Test nested expansion
-    Config.set('key/path', '$(key/path)value')
+    assert Config.set('key/path', '$(key/path)value')
     assert Config.expand('$(key/path)') == 'value'
     # Test multiple nested expansion
-    Config.set('key/path1', '$(key/path3)value')
-    Config.set('key/path2', '$(key/path1)value')
-    Config.set('key/path3', '$(key/path2)value')
+    assert Config.set('key/path1', '$(key/path3)value')
+    assert Config.set('key/path2', '$(key/path1)value')
+    assert Config.set('key/path3', '$(key/path2)value')
     assert Config.expand('$(key/path1)') == 'valuevaluevalue'
     assert Config.expand('$(key/path2)') == 'valuevaluevalue'
     assert Config.expand('$(key/path3)') == 'valuevaluevalue'
@@ -39,7 +39,7 @@ def test_config_set_expand():
 # Configuration set/exists test
 def test_config_set_exists():
     """Configuration set/exists test"""
-    Config.set('/path/to/key', 'value')
+    assert Config.set('/path/to/key', 'value')
     assert Config.exists('/path/to/key')
     assert not Config.exists('/path/to/no/key')
 
@@ -47,7 +47,18 @@ def test_config_set_exists():
 # Configuration set/remove test
 def test_config_set_remove():
     """Configuration set/remove test"""
-    Config.set('key/path', 'value')
+    assert Config.set('key/path', 'value')
     assert Config.get('key/path', '') != ''
-    Config.remove('key/path')
+    assert Config.remove('key/path')
+    assert Config.remove('key/path/not/here')
     assert Config.get('key/path', 'default') == 'default'
+
+
+# Configuration invalid key test
+def test_config_invalid_key():
+    """Configuration invalid key test"""
+    assert not Config.set('', 'value')
+    assert Config.set('/valid/key', 'value')
+    assert not Config.set('/valid/key/invalid', 'new value')
+    assert not Config.set('/path/user', 'this can not be set!')
+    assert not Config.set('path/config', 'this can not be set either...')
