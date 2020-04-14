4b60ee5890a5ed49acfa81e8c500aef9d0463949
Isaac Bedard <ibedard@biomedscienceacademy.org>
2020-04-13 20:55:09 -0400

    Fix mistakes in pluginspanel.py
    Oops, had some mixed whitespace types.
    Also, don't know what was going on with all of the "self" references. Should have been instance.
    
    Also, javascritpt ternary syntax does not work in python

--- meg_runtime/ui/pluginspanel.py
+++ meg_runtime/ui/pluginspanel.py
@@ -1,10 +1,8 @@
 
 from PyQt5 import QtWidgets
 
-from meg_runtime.config import Config
 from meg_runtime.ui.basepanel import BasePanel
-from meg_runtime.ui.filechooser import FileChooser
-from meg_runtime.ui.helpers import PanelException
+from meg_runtime.logger import Logger
 
 
 class PluginsPanel(BasePanel):
@@ -13,25 +11,29 @@ class PluginsPanel(BasePanel):
     def __init__(self, manager, **kwargs):
         super().__init__(**kwargs)
 
+    def get_title():
+        """Get the title of this panel."""
+        return 'Plugins'
+
     def on_load():
         """Load dynamic elements within the panel."""
 
         # Attach handlers
-        self.enable_button = self.findChild(QtWidgets.QPushButton, 'enableButton')
+        instance = self.get_widgets()
+        self.enable_button = instance.findChild(QtWidgets.QPushButton, 'enableButton')
         #self.enable_button.clicked.connect()
-        self.disable_button = self.findChild(QtWidgets.QPushButton, 'disableButton')
+        self.disable_button = instance.findChild(QtWidgets.QPushButton, 'disableButton')
         #self.disable_button.clicked.connect()
-        self.uninstall_button = self.findChild(QtWidgets.QPushButton, 'uninstallButton')
+        self.uninstall_button = instance.findChild(QtWidgets.QPushButton, 'uninstallButton')
         #self.uninstall_button.clicked.connect()
-        self.plugin_list = self.findChild(QtWidgets.QTreeWidget, 'pluginList')
+        self.plugin_list = instance.findChild(QtWidgets.QTreeWidget, 'pluginList')
 
-        instance = PluginsPanel.get_instance()
         # Add the file viewer/chooser
         pluginManager = PluginManager.get_instance();
-		plugins = pluginManager.get_all()
+        plugins = pluginManager.get_all()
         pluginItems = [
             QtWidgets.QTreeWidgetItem([
-                plugin.enabled() ? 🔵 : ⚪
+                '🔵' if plugin.enabled()  else '⚪' 
             ])
             for plugin in plugins
         ]
